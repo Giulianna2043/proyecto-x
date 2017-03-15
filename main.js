202ec56171b6063a1fb4900b9046248cd25301b2
giuliana <j.bustinza42@gmail.com>
2017-03-15 13:39:52 -0500

    avance del profe

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//Lógica
+//Lógica objeto
 function Chat(_nombre, _imagen,mensaje,hora)
 {
 	this.nombre =  _nombre;
@@ -18,10 +18,11 @@ var dataListaChats = [
     new Chat("bart", 'image/bart.jpg',"buscando inmformacion","10:35"),
     new Chat("bart", 'image/bart.jpg',"buscando inmformacion","10:35"),
     new Chat("bart", 'image/bart.jpg',"buscando inmformacion","10:35"),
-	
+
 ];
 
 //Parte visual
+//liListItem = document.createElement('LI');
 var liListItem = null;
 function init()
 {
@@ -32,7 +33,7 @@ function initChatList() {
 	var elListaChats = document.getElementById("lista-chats");
 
 	for (var i in dataListaChats) {
-		var htmlChatItem = '<li><div class="avatar">' +
+		var htmlChatItem = '<li draggrable="true" ><div class="avatar">' +
 			'<img src="' + dataListaChats[i].imagenURL +  '" alt="" class="wh-44">' +
 			'<h4 class="w-contact-name">' + dataListaChats[i].nombre + '</h4>' +
 			'<p class="w-last-message" id="mensaje">' + dataListaChats[i].ultimoMensaje + '</p>' +
@@ -61,7 +62,7 @@ function onChatItemClick(evt) {
 	var contactName = evt.currentTarget.getElementsByClassName('w-contact-name')[0].textContent;
 	var imgURL = evt.currentTarget.getElementsByClassName('wh-44')[0].src;
 	console.log('click');
-	
+
     crearListaChats();
 }
 
@@ -75,7 +76,7 @@ function onMensajeKey(evt){
 		elInputMensajes.value = "";
 	}
 }
-    
+
 function crearMensaje(_mensaje) {
 	var htmlMensajeIn = '<div class="w-message w-message-in">' +
 		'<div class="w-message-text">' +
@@ -100,15 +101,20 @@ function crearMensaje(_mensaje) {
 
 
 	var elChat = document.getElementById("chat");
+
 	elChat.innerHTML += htmlMensajeOut;
+//agregaqndo...
+	alert(olaa);
+	socket.emit('send',_mensaje);
+	//fin
 	elChat.scrollTop = elChat.scrollHeight;
 }
-    
+
 function crearListaChats(){
 var chat=document.getElementById("chat");
     chat.innerHTML="";
 }
-    
+
 function crearChat(_mensaje){
 	var elListaChats = document.getElementById("lista-chats");
     var d = new Date();
@@ -126,23 +132,23 @@ function crearChat(_mensaje){
 		elListaChats.insertBefore(liListItem, elListaChats.childNodes[0]);
 	}
 	setEventsChatList();
-    
+
 	//elListaChats.innerHTML += htmlChatItem;
 }
-    
+
 function actualizarCabeceraChat(_contactName, _imageURL, _estado){
 	var chatHeader = document.getElementById("chat-header");
 	chatHeader.getElementsByClassName('w-contact-name')[0].innerHTML = _contactName;
 	chatHeader.getElementsByClassName('w-users-messages')[0].innerHTML = _estado;
 	chatHeader.getElementsByTagName('img')[0].src = _imageURL;
-} 
+}
 
 /*function actualizarChat(_contactName){
 	var chat = document.getElementById("chat");
 	chat.getElementsByClassName('w-contact-name')[0].innerHTML = _contactName;
 	chatHeader.getElementsByClassName('w-users-messages')[0].innerHTML = _estado;
 	chatHeader.getElementsByTagName('img')[0].src = _imageURL;
-} */ 
+} */
 
 
 
@@ -150,45 +156,15 @@ function actualizarCabeceraChat(_contactName, _imageURL, _estado){
     var search = document.getElementById("search");
     var contacto = document.getElementsByTagName("h4");
     var forEach = Array.prototype.forEach;
- 
+
  search.addEventListener("keyup", function(e){
    var choice = this.value;
- 
+
    forEach.call(contacto, function(f){
        if (f.innerHTML.toLowerCase().search(choice.toLowerCase()) == -1)
-           f.parentNode.parentNode.style.display = "none";   
+           f.parentNode.parentNode.style.display = "none";
        else
-          f.parentNode.parentNode.style.display = "block";        
+          f.parentNode.parentNode.style.display = "block";
    });
- }, 
+ },
  false);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
